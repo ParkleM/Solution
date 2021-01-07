a81f90db421d2eb8114154032edb291b1a6af892
ParkleM <gerryniga@gmail.com>
2021-01-07 18:12:30 +0300

    Чучуть переделОчка в мейне и втором

--- Task2/GUI2/Form2.Designer.cs
+++ Task2/GUI2/Form2.Designer.cs
@@ -31,7 +31,6 @@ namespace GUI2
         {
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.button2 = new System.Windows.Forms.Button();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
@@ -51,7 +50,7 @@ namespace GUI2
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(215, 20);
             this.textBox1.TabIndex = 0;
-            this.textBox1.Text = "C:\\Users\\yakov\\Desktop\\Solution\\Task2\\Example.txt";
+            this.textBox1.Text = "C:\\Users\\mrtro\\Desktop\\Solution — копия (3)\\Task2\\Example.txt";
             // 
             // label1
             // 
@@ -62,23 +61,13 @@ namespace GUI2
             this.label1.TabIndex = 1;
             this.label1.Text = "Путь к текстовому файлу:";
             // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(12, 85);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(215, 30);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "Выбрать";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
             // textBox2
             // 
             this.textBox2.Location = new System.Drawing.Point(233, 25);
             this.textBox2.Multiline = true;
             this.textBox2.Name = "textBox2";
             this.textBox2.ReadOnly = true;
-            this.textBox2.Size = new System.Drawing.Size(215, 246);
+            this.textBox2.Size = new System.Drawing.Size(215, 210);
             this.textBox2.TabIndex = 3;
             // 
             // label2
@@ -93,7 +82,7 @@ namespace GUI2
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(9, 118);
+            this.label3.Location = new System.Drawing.Point(9, 82);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(179, 13);
             this.label3.TabIndex = 5;
@@ -102,7 +91,7 @@ namespace GUI2
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(9, 196);
+            this.label4.Location = new System.Drawing.Point(9, 160);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(135, 13);
             this.label4.TabIndex = 6;
@@ -111,7 +100,7 @@ namespace GUI2
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(9, 157);
+            this.label5.Location = new System.Drawing.Point(9, 121);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(154, 13);
             this.label5.TabIndex = 7;
@@ -120,7 +109,7 @@ namespace GUI2
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(9, 235);
+            this.label6.Location = new System.Drawing.Point(9, 199);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(110, 13);
             this.label6.TabIndex = 8;
@@ -128,7 +117,7 @@ namespace GUI2
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(12, 134);
+            this.textBox3.Location = new System.Drawing.Point(12, 98);
             this.textBox3.Name = "textBox3";
             this.textBox3.ReadOnly = true;
             this.textBox3.Size = new System.Drawing.Size(215, 20);
@@ -136,7 +125,7 @@ namespace GUI2
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(12, 173);
+            this.textBox4.Location = new System.Drawing.Point(12, 137);
             this.textBox4.Name = "textBox4";
             this.textBox4.ReadOnly = true;
             this.textBox4.Size = new System.Drawing.Size(215, 20);
@@ -144,7 +133,7 @@ namespace GUI2
             // 
             // textBox5
             // 
-            this.textBox5.Location = new System.Drawing.Point(12, 212);
+            this.textBox5.Location = new System.Drawing.Point(12, 176);
             this.textBox5.Name = "textBox5";
             this.textBox5.ReadOnly = true;
             this.textBox5.Size = new System.Drawing.Size(215, 20);
@@ -152,7 +141,7 @@ namespace GUI2
             // 
             // textBox6
             // 
-            this.textBox6.Location = new System.Drawing.Point(12, 251);
+            this.textBox6.Location = new System.Drawing.Point(12, 215);
             this.textBox6.Name = "textBox6";
             this.textBox6.ReadOnly = true;
             this.textBox6.Size = new System.Drawing.Size(215, 20);
@@ -172,7 +161,7 @@ namespace GUI2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(460, 283);
+            this.ClientSize = new System.Drawing.Size(460, 244);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox6);
             this.Controls.Add(this.textBox5);
@@ -184,7 +173,6 @@ namespace GUI2
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.button2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
@@ -201,7 +189,6 @@ namespace GUI2
 
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button2;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
